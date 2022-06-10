a5e3e7430ab371e9a87821b483b277e6bdc4f5df
babynicky <jackthelion83@gmail.com>
2022-06-10 00:12:30 -0500

    Works!

--- app/controllers/staff_dates_controller.rb
+++ app/controllers/staff_dates_controller.rb
@@ -1,10 +1,9 @@
 class StaffDatesController < ApplicationController
   before_action :set_staff_date, only: %i[ show edit update destroy send_email]
-  before_action :set_employee
 
 
   def send_email
-    StaffMailer.with(employees: @staff_date.employees).send_staff.deliver_later
+    StaffMailer.with(employees: @staff_date.employees).send_staff.deliver_now
     # flash.now[:notice] = "Staff was successfully sent"
     head :ok
   end
@@ -70,10 +69,6 @@ class StaffDatesController < ApplicationController
 
     def staff_date_params
       params.require(:staff_date).permit(:date)
-    end
-
-    def set_employee
-      @employee = @staff_date.employees.all
     end
 
     def employee_params
