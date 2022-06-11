7787112b75d1358b82e1bec7b24cc041ad0cf5e5
BabyNicky <jackthelion83@gmail.com>
2022-06-10 23:54:19 -0600

    "small improvements"

--- test/mailers/previews/staff_mailer_preview.rb
+++ test/mailers/previews/staff_mailer_preview.rb
@@ -1,8 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/staff_mailer
 class StaffMailerPreview < ActionMailer::Preview
-    def send_email
-        
-        
-        StaffMailer.with(employees: @staff_date.employees).send_staff
-      end
+    # def send_email      
+    #   StaffMailer.with(employees: @staff_date.employees).send_staff
+    # end
 end
