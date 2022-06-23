e71382c2db4c4301beb1ab248e8d6fb844877e5e
BabyNicky <jackthelion83@gmail.com>
2022-06-23 06:02:26 -0600

    Update Styling

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -3,7 +3,4 @@ class Employee < ApplicationRecord
 
   validates :name, presence: true
   validates :employee_number, presence: true, numericality: { only_integer: true, greater_than: 0 }
-  
-  
-  # broadcasts_to ->(staff_date) { "staff_dates" }, inserts_by: :prepend
 end
