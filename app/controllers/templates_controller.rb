9873905bfd9d0890e2abd047b28a58c6899b0c9e
babynicky <jackthelion83@gmail.com>
2022-08-04 02:40:03 -0500

    It_works!

--- app/controllers/templates_controller.rb
+++ app/controllers/templates_controller.rb
@@ -10,11 +10,11 @@ class TemplatesController < ApplicationController
     @template = current_user.templates.create!(template_params)
     
     @template.name = current_user.name
-    @template.employees = [employees]
     
-    # employees.each do |employee|
-    #   @template.employees = [employee] 
-    # end
+    employees.each do |employee|
+      @template.employees << [employee] 
+    end
+   
     @template.save
   end 
 
